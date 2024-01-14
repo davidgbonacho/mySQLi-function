7ef6ce8fcb9a3a677e11810ff49e17eaf2ef5c82
Tizedit <comunicacion@tizedit.com>
2024-01-14 13:15:51 +0100

    C

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>execute SQL query</title>
+    <title>Execute SQL query</title>
 </head>
 <body>
 
